e47a62114f61fc4b6dbe40c164c4f4d5feb65574
cn0379 <cn0379@outlook.com>
2020-08-22 00:53:18 +0800

    admin-element

---
+++ vue-admin-template-master/src/views/meterial/news/demo/demo.vue
@@ -0,0 +1,20 @@
+<!--  -->
+<template>
+    <div>
+    vue
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
